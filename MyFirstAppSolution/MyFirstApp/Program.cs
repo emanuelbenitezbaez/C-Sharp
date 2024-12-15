bf9b7478d4676304153a106274d02945d7e9ff79
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2024-12-15 17:54:54 -0300

    My first App

---
+++ MyFirstAppSolution/MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace MyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from my first App!");
+            
+        }
+    }
+}
---
+++ MyFirstAppSolution/MySecondApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MySecondApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from my second App!");
+        }
+    }
+}
