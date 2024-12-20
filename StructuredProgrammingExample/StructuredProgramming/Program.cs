bb6b0fe1ebe4107502276d59ea4f05bc7480fbb9
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2024-12-20 11:54:40 -0300

    Structured Programming

--- StructuredProgrammingExample/StructuredProgramming/Program.cs
+++ StructuredProgrammingExample/StructuredProgramming/Program.cs
@@ -10,51 +10,66 @@ namespace StructuredProgramming
         {
             //Realizar una calculadora que reciba 2 numeros y realice las operaciones basicas.
 
-            int resultado = 0;
-            int number1;
-            int number2;
+            double resultado = 0;
+            double number1;
+            double number2;
             int operation;
 
 
-            
+           
 
-            operation = GetIntegerDataUser ("Ingrese la operacion a realizar: 1. Suma 2. Resta 3. Multiplicacion 4. Division");
 
-            switch (operation)
+            do
             {
-                case 1:
-                    number1 = GetIntegerDataUser("Ingrese el primer numero");
-                    number2 = GetIntegerDataUser("Ingrese el segundo numero");
-                    resultado = number1 + number2;
-                    Console.WriteLine("El resultado de la suma es: " + resultado);
-                    break;
-                case 2:
-                    number1 = GetIntegerDataUser("Ingrese el primer numero");
-                    number2 = GetIntegerDataUser("Ingrese el segundo numero");
-                    resultado = number1 - number2;
-                    break;
-                case 3:
-                    number1 = GetIntegerDataUser("Ingrese el primer numero");
-                    number2 = GetIntegerDataUser("Ingrese el segundo numero");
-                    resultado = number1 * number2;
-                    break;
-                case 4:
-                    number1 = GetIntegerDataUser("Ingrese el primer numero");
-                    number2 = GetIntegerDataUser("Ingrese el segundo numero");
-                    if (number2 == 0)
-                    {
-                        Console.WriteLine("No se puede dividir por 0");
-                    }
-                    else
-                    {
-                        resultado = number1 / number2;
-                        Console.WriteLine("El resultado de la division es: " + resultado);
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Operacion no valida");
-                    break;
-            }
+                
+                operation = GetIntegerDataUser("Ingrese la operacion a realizar:  1. Suma 2. Resta 3. Multiplicacion 4. Division  0. Para salir ");
+                
+                switch (operation)
+                {
+                    case 1:
+                        number1 = GetDoubleDataFromUser("Ingrese el primer numero");
+                        number2 = GetDoubleDataFromUser("Ingrese el segundo numero");
+                        resultado = number1 + number2;
+                        Console.WriteLine($"El resultado de sumar {number1} + {number2} es: {resultado}");
+                        break;
+                    case 2:
+                        number1 = GetDoubleDataFromUser("Ingrese el primer numero");
+                        number2 = GetDoubleDataFromUser("Ingrese el segundo numero");
+                        resultado = number1 - number2;
+                        Console.WriteLine($"El resultado de restar {number1} - {number2} es: {resultado}");
+                        break;
+                    case 3:
+                        number1 = GetDoubleDataFromUser("Ingrese el primer numero");
+                        number2 = GetDoubleDataFromUser("Ingrese el segundo numero");
+                        resultado = number1 * number2;
+                        Console.WriteLine($"El resultado de multiplicar {number1 * number2} es: {resultado}");
+                        break;
+                    case 4:
+                        number1 = GetDoubleDataFromUser("Ingrese el primer numero");
+                        number2 = GetDoubleDataFromUser("Ingrese el segundo numero");
+                        if (number2 == 0)
+                        {
+                            Console.WriteLine("No se puede dividir por 0");
+                        }
+                        else
+                        {
+                            resultado = number1 / number2;
+                            Console.WriteLine($"El resultado de dividir {number1} / {number2} es: {resultado}");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Operacion no valida");
+                        break;
+
+                       
+                }
+                
+            } while (operation != 0);
+
+            Console.Clear();
+
+
+
 
 
 
@@ -62,12 +77,37 @@ namespace StructuredProgramming
 
         }
 
+        private static double GetDoubleDataFromUser(string operation)
+        {
+            bool IsValid = false;
+            //El signo de interrogacion indica que la variable puede recibir un valor nulo.(TryParse puede devovler nulo)
+            string? userData;
+            double data = 0;
+            while (!IsValid)
+            {
+                Console.WriteLine(operation);
+                userData = Console.ReadLine();
+
+                if (!double.TryParse(userData, out data))
+                {
+                    Console.WriteLine("El valor ingresado no es un numero");
+                }
+                else
+                {
+                    IsValid = true;
+                }
+
+            }
+            return data;
+        }
+
 
         //Funcion que valida y obtiene un numero entero ingresado por el usuario
         private static int GetIntegerDataUser(string operation)
         {
             bool IsValid = false;
-            string userData;
+            //El signo de interrogacion indica que la variable puede recibir un valor nulo.(TryParse puede devovler nulo)
+            string? userData;
             int data = 0;
             while (!IsValid)
             {
