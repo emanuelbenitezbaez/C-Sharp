2edf956236130d6b2173dccf9ff54e9ec7d8a929
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2024-12-23 12:32:08 -0300

    excepciones

---
+++ ManejorDeErroresYDepuracion/ManejorDeErroresYDepuracion/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ManejorDeErroresYDepuracion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                divide();
+
+            } catch (Exception ex) {
+                throw new Exception("Error al dividir", ex);
+            }
+            finally
+            {
+                Console.WriteLine("El programa termino de ejecutarse");
+            }
+
+
+
+        }
+
+        private static void divide()
+        {
+            Console.WriteLine("Ingrese un numero para dividir 100");
+            string input = Console.ReadLine();
+            int divisor;
+            bool isValid = int.TryParse(input, out divisor);
+            if (!isValid)
+            {
+                divisor = 0;
+            }
+
+            int result = 100 / divisor;
+            Console.WriteLine($"Resultado: {result}");
+        }
+    } 
+}
