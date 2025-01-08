0a6eded783a735292ce5cfc50762de37e8c21413
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2025-01-08 11:08:25 -0300

    empty string

--- InputOutput/StringLiteral/Program.cs
+++ InputOutput/StringLiteral/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            //  String literal , string interpolation
+            //  String literal  
 
             string path = "C:\\Users\\benit\\Downloads\\PKO";
             Console.WriteLine(path);
@@ -12,14 +12,32 @@
             string pathWellDeclared = @"C:\\Users\\benit\\Downloads\\PKO";
             Console.WriteLine(pathWellDeclared);
 
-            string name = "Emanuel";
 
+            //String interpolation
+
+            string name = "Emanuel";
             int age = 35;
 
             Console.WriteLine("Your name is " + name + ", and you are " + age);
             Console.WriteLine("Your name is {0}, and you are {1}" , name , age);
-
             Console.WriteLine($"Your name is {name}, and you are {age}.");
+
+            // Empty string
+
+            Console.WriteLine("Ingresa tu profesion:");
+
+            string profesion = Console.ReadLine();
+
+            
+
+            if (profesion != string.Empty)
+            {
+                Console.WriteLine($"Your profesion is {profesion}");
+            }
+            else
+            {
+                Console.WriteLine("Empty");
+            }
 
         }
     }
--- InputOutput/StringLiteral/obj/Debug/net8.0/StringLiteral.AssemblyInfo.cs
+++ InputOutput/StringLiteral/obj/Debug/net8.0/StringLiteral.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StringLiteral")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a646bbc4b7e0a5c21e890e8b6db120c76811990")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17b03ac8d225d5469c559804aeb6b962b19812d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("StringLiteral")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StringLiteral")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
