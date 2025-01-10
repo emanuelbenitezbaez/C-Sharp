41f304b68696073fb19332d5470ac792753d7834
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2025-01-10 17:46:11 -0300

    arrays

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -15,8 +15,27 @@
             int[] array4 = new int[4] {1,2,3,4};
             int[] array5 = new int[5];
 
-          
 
+            //CARGA DE ARRAY DE ENTEROS
+            for (int i = 0; i < array5.Length; i++)
+            {
+                Console.WriteLine("Ingrese un entero:");
+
+                while (!int.TryParse(Console.ReadLine(), out  array5[i]))
+                {
+                    Console.WriteLine("No valido");
+                }
+
+            }
+
+            for (int i = 0;i < array5.Length; i++)
+            {
+                Console.WriteLine($"Indice {i}: {array5[i]}");
+            }
+
+
+
+            //CARGA DE ARRAY DE STRINGS
             string[] arregloCadena = new string[3];
 
             for (int i = 0;i <arregloCadena.Length;i++)
