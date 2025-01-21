f6ddd847be0255b86d1ae8bd10fd42c705d51a5a
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2025-01-21 12:14:40 -0300

    arrays

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -13,8 +13,12 @@
             int[] array2 = new int[] { 1, 2,3 };
             int[] array3 = [1,2,3,4];
             int[] array4 = [1,2,3,4];
-            int[] array5 = new int[5];
+            
 
+            Console.WriteLine("Cuantos enteros desea ingresar?");
+            int cantidad = int.Parse(Console.ReadLine());
+
+            int[] array5 = new int[cantidad];
 
             //CARGA DE ARRAY DE ENTEROS
             for (int i = 0; i < array5.Length; i++)
--- Arrays/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
+++ Arrays/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d847a426fd767c3eef81d0f038ac8c41eebe82c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0b5e6dccc3637ce4ece9a3f2557d3872e25511d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
