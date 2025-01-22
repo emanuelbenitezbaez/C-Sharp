6c8e59ba0ddbacfc952c57a78a8e73e4f399ced7
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2025-01-22 10:36:19 -0300

    suma de angulos

---
+++ Arrays/ArraysAngleEx/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ArraysAngleEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int angleCount = 3;
+            int[] angles = new int[angleCount];
+
+            for(int i = 0; i < angleCount; i++)
+            {
+                Console.WriteLine("Enter angle " + (i + 1) + ": ");
+                angles[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int angleSum = 0;
+
+            foreach (int angle in angles)
+            {
+                angleSum += angle;
+            }
+
+            Console.WriteLine($"Sum of angles : {angleSum}");
+
+            if (angleSum == 180)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            //Console.WriteLine(angleSum == 180? "Valid" : "Invalid");
+        }
+    }
+}
--- Arrays/ArraysAngleEx/obj/Debug/net8.0/ArraysAngleEx.AssemblyInfo.cs
+++ Arrays/ArraysAngleEx/obj/Debug/net8.0/ArraysAngleEx.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ArraysAngleEx")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b635a38ddb48ca2d4af1eb328f91ff78dce03316")]
-[assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
+[assembly: System.Reflection.AssemblyProductAttribute("ArraysAngleEx")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ArraysAngleEx")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
