ac9d79ddf7bf75b04d2fa16768aa6b628a55e848
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2025-01-23 16:19:07 -0300

    Creacion del conecto de base de datos con entity

--- WebAppEmployeeCrudMvc/WebAppEmployeeCrudMvc/Data/AppDbContext.cs
+++ WebAppEmployeeCrudMvc/WebAppEmployeeCrudMvc/Data/AppDbContext.cs
@@ -1,6 +1,41 @@
-﻿namespace WebAppEmployeeCrudMvc.Data
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppEmployeeCrudMvc.Models;
+namespace WebAppEmployeeCrudMvc.Data
 {
-    public class AppDbContext
+    // Esta clase define el contexto de base de datos de la aplicación
+    public class AppDbContext : DbContext
     {
+        // Constructor que recibe opciones para configurar el contexto
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Define un conjunto de datos (tabla) para la entidad Employee
+        public DbSet<Employee> Employees { get; set; }
+
+        // Método para configurar detalles adicionales de las entidades
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Define la configuración de la entidad Employee
+            modelBuilder.Entity<Employee>(table =>
+            {
+                // Define la clave primaria de la entidad Employee
+                table.HasKey(col => col.IdEmployee);
+
+                // Configura la propiedad IdEmployee para que
+                // sea una columna de identidad (auto incremental)
+                table.Property(col => col.IdEmployee)
+                    .UseIdentityColumn()
+                    .ValueGeneratedOnAdd();
+
+                // Configura la propiedad FullName con un máximo de 50 caracteres
+                table.Property(col => col.FullName).HasMaxLength(50);
+            });
+
+            // Especifica que la entidad Employee debe mapearse a la tabla "Employee"
+            modelBuilder.Entity<Employee>().ToTable("Employee");
+        }
     }
+
+
 }
