3fa4eb35c0214bb44bf7fe7c05e01c35fd8798c3
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2025-01-27 14:56:48 -0300

    todo refactorizado

--- ToDo/ToDo/Program.cs
+++ ToDo/ToDo/Program.cs
@@ -25,52 +25,77 @@ namespace ToDo
                 Console.WriteLine("3 - Ver tarea.");
                 Console.WriteLine("0 - Salir.");
 
-                int option = Convert.ToInt32(Console.ReadLine());
 
-                switch (option)
+
+                try
                 {
-                    case 0:
-                        isValid = false;
-                        break;
-                    case 1:
-                        Console.WriteLine("Ingrese la tarea a agregar:");
-                        string? task = Console.ReadLine();
-                        if (!string.IsNullOrEmpty(task))
-                        {
-                            tasks.Add(task);
-                            Console.WriteLine("Tarea agregada!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("La tarea no puede estar vacía.");
-                        }
-                        break;
-                    case 2:
-                        Console.WriteLine("Ingrese la tarea a borrar:");
-                        string? taskToDelete = Console.ReadLine();
-                        if (!string.IsNullOrEmpty(taskToDelete))
-                        {
-                            tasks.Remove(taskToDelete);
-                            Console.WriteLine("Tarea borrada!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("La tarea no puede estar vacía.");
-                        }
-                        break;
 
-                    case 3:
-                        Console.WriteLine("Tareas:");
-                       
-                        for(int i = 0;i < tasks.Count; i++)
-                        {
-                            Console.WriteLine(i + " - " + tasks[i]);
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("Opción no válida.");
-                        break;
+                    int option = Convert.ToInt32(Console.ReadLine());
+
+                    switch (option)
+                    {
+                        case 0:
+                            isValid = false;
+                            break;
+                        case 1:
+                            Console.WriteLine("Ingrese la tarea a agregar:");
+                            string? task = Console.ReadLine();
+                            if (!string.IsNullOrEmpty(task))
+                            {
+                                tasks.Add(task);
+                                Console.WriteLine("Tarea agregada!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("La tarea no puede estar vacía.");
+                            }
+                            break;
+                        case 2:
+
+                            Console.WriteLine("Ingrese el número de la tarea a borrar:");
+                            if (int.TryParse(Console.ReadLine(), out int index) && index >= 0 && index < tasks.Count)
+                            {
+                                tasks.RemoveAt(index);
+                                Console.WriteLine("Tarea borrada!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Número de tarea no válido.");
+                            }
+
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Tareas:");
+
+                        
+
+
+                            if (tasks.Count == 0)
+                            {
+                                Console.WriteLine("No hay tareas por hacer.");
+                            }
+                            else
+                            {
+                                for (int i = 0; i < tasks.Count; i++)
+                                {
+                                    Console.WriteLine(i + " - " + tasks[i]);
+                                }
+                            }
+                            break;
+
+                        default:
+                            Console.WriteLine("Opción no válida.");
+                            break;
+                    }
+
                 }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error: Debes ingresar un número válido.");
+                }
+
+
             }
         }
     }
--- ToDo/ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
+++ ToDo/ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ac1ed33871f4a5a8c9806d2593e46fd4b13d754")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7e21c0958566a6d13fa4fbf520056ed57894e37")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
