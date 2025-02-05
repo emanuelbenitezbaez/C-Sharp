255b11ebe505b1d2234426c7f95030900303ecb1
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2025-02-05 12:40:43 -0300

    number of digits

---
+++ numberOfDigits/numberOfDigits/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace numberOfDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            numberOfDigits(11);
+        }
+
+        private static void numberOfDigits(int number)
+        {
+
+            int numer;
+
+
+           
+
+
+            if (number > 0 && number < 10)
+            {
+                Console.WriteLine("Numero de un digito");
+            }
+            else if (number >= 10 && number < 100)
+            {
+                Console.WriteLine("Numero de dos digitos");
+
+            }
+            else
+            {
+                Console.WriteLine("Numero tiene 3 digitos");
+            }
+
+        }
+    }
+}
